9aa3fe82d8deb87924f2a063873765952356748f
Lee-Orr <lee-orr@users.noreply.github.com>
2024-04-22 22:22:33 +0000

    simple movement

--- game_client/src/main.rs
+++ game_client/src/main.rs
@@ -1,34 +1,24 @@
 //! A simple 3D scene with light shining over a cube sitting on a plane.
 
 use bevy::prelude::*;
+use bevy_mod_picking::prelude::*;
+use game_server_logic::{Clicked, GameServerLogic, GridPosition, SelectedUnit, Tile, Unit};
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPickingPlugins)
+        .add_plugins(GameServerLogic)
         .add_systems(Startup, setup)
+        .add_systems(PostUpdate, (draw_tiles,draw_units, move_units, display_selection))
+        .add_systems(Last, make_pickable)
         .run();
 }
 
 /// set up a simple 3D scene
 fn setup(
     mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-    asset_server: Res<AssetServer>,
 ) {
-    // circular base
-    commands.spawn(SceneBundle {
-        scene: asset_server.load("client_assets/kenney-hexagon-kit/GLB format/sand.glb#Scene0"),
-        transform: Transform::from_xyz(0.0, -0.5, 0.0).with_scale(Vec3::splat(2.)),
-        ..default()
-    });
-    // cube
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(Cuboid::new(1.0, 1.0, 1.0)),
-        material: materials.add(Color::rgb_u8(124, 144, 255)),
-        transform: Transform::from_xyz(0.0, 0.5, 0.0),
-        ..default()
-    });
     // light
     commands.spawn(PointLightBundle {
         point_light: PointLight {
@@ -43,4 +33,123 @@ fn setup(
         transform: Transform::from_xyz(-2.5, 4.5, 9.0).looking_at(Vec3::ZERO, Vec3::Y),
         ..default()
     });
+}
+
+fn draw_tiles(
+    mut commands: Commands,
+    tiles: Query<(Entity, &Tile, &GridPosition), Without<Handle<Scene>>>,
+    asset_server: Res<AssetServer>,
+) {
+    for (entity, tile, transform) in &tiles {
+        let asset = match tile {
+            Tile::FlatGround => "tile_dirt",
+            Tile::Rocks => "tile_rock",
+            Tile::Forest => "tile_tree",
+        };
+        let current_transform = transform.clone();
+        commands.entity(entity).insert((
+            SceneBundle {
+                transform: Transform::from_xyz(transform.x as f32, 0.0, transform.y as f32),
+                scene: asset_server.load(format!(
+                    "client_assets/kenney-tower-defense/Models/GLTF format/{asset}.glb#Scene0"
+                )),
+                ..Default::default()
+            },
+            PickableBundle::default(),
+            On::<Pointer<Click>>::run(move |event: Listener<Pointer<Click>>, mut writer: EventWriter<Clicked>| {
+                let position_normalized = event.hit.position.unwrap_or(Vec3::splat(-0.5)) + 0.5;
+                let position_normalized = position_normalized - position_normalized.floor();
+                let x_offset = (position_normalized.x * u8::MAX as f32) as u8;
+                let y_offset = (position_normalized.z * u8::MAX as f32) as u8;
+                writer.send(Clicked::Tile { x: current_transform.x, y: current_transform.y, x_offset, y_offset });
+            }),
+        ));
+    }
+}
+
+fn draw_units(
+    mut commands: Commands,
+    units: Query<(Entity, &Unit, &GridPosition), Without<Handle<Mesh>>>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    if units.is_empty() {
+        return;
+    }
+    let red = materials.add(Color::RED);
+    let blue = materials.add(Color::BLUE);
+    let mesh = meshes.add(Cuboid::new(0.5, 1.0, 0.5));
+    for (entity, unit, transform) in &units {
+        let material = match unit {
+            Unit::Blue => blue.clone(),
+            Unit::Red => red.clone(),
+        };
+        let current = entity;
+        commands.entity(entity).insert((
+            PbrBundle {
+                mesh: mesh.clone(),
+                material,
+                transform: Transform::from_xyz(transform.x as f32, 0.6, transform.y as f32),
+                ..default()
+            },
+            PickableBundle::default(),
+            On::<Pointer<Click>>::run(move |mut writer: EventWriter<Clicked>, query: Query<&GridPosition>| {
+                let Ok(transform) = query.get(current) else {
+                    return;
+                };
+                writer.send(Clicked::Unit { x: transform.x, y: transform.y, entity });
+            }),
+        ));
+    }
+}
+
+fn move_units(mut commands: Commands,
+    units: Query<(Entity, &GridPosition), Changed<GridPosition>>) {
+    for (entity, position) in &units {
+        commands.entity(entity).insert(Transform::from_xyz(position.x as f32, 0.6, position.y as f32));
+    }
+}
+
+#[derive(Component)]
+struct SelectionMarker(Entity);
+
+fn display_selection(mut commands: Commands, 
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    selected: Query<Entity, Added<SelectedUnit>>, existing_selections: Query<&SelectionMarker>, mut removed: RemovedComponents<SelectedUnit>) {
+    for selected in &selected {
+        let mut child = None;
+        commands.entity(selected).with_children(|p| {
+            let material = materials.add(Color::GREEN);
+            let mesh: Handle<Mesh> = meshes.add(Cuboid::new(0.2, 0.2, 0.2));
+   
+            let id = p.spawn(PbrBundle {
+                transform: Transform::from_xyz(0., 1.0, 0.),
+                mesh,
+                material,
+                ..Default::default()
+            }).id();
+            child = Some(id);
+        });
+        if let Some(child) = child {
+            commands.entity(selected).insert(SelectionMarker(child));
+        }
+    }
+
+    for removed in removed.read() {
+        if let Ok(selection) = existing_selections.get(removed) {
+            commands.entity(selection.0).despawn_recursive();
+        }
+    }
+}
+
+fn make_pickable(
+    mut commands: Commands,
+    meshes: Query<Entity, (With<Handle<Mesh>>, Without<Pickable>)>,
+) {
+    for entity in meshes.iter() {
+        commands
+            .entity(entity)
+            .insert(PickableBundle::default());
+    }
 }
--- game_server_logic/src/lib.rs
+++ game_server_logic/src/lib.rs
@@ -1,14 +1,82 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use bevy::prelude::*;
+
+pub struct GameServerLogic;
+
+impl Plugin for GameServerLogic {
+    fn build(&self, app: &mut App) {
+        app
+            .add_event::<Clicked>()
+            .add_systems(Startup, spawn_map)
+            .add_systems(Update, process_click);
+    }
+}
+
+#[derive(Component, Clone, Debug, Reflect)]
+pub struct SelectedUnit;
+
+#[derive(Component, Clone, Debug, Reflect)]
+pub enum Tile {
+    FlatGround,
+    Rocks,
+    Forest
+}
+
+#[derive(Component, Clone, Debug, Reflect)]
+pub enum Unit {
+    Blue,
+    Red
+}
+
+#[derive(Event, Clone, Debug, Reflect)]
+pub enum Clicked {
+    Unit { x: u32, y: u32, entity: Entity },
+    Tile { x: u32, y: u32, x_offset: u8, y_offset: u8 }
+}
+
+#[derive(Component, Clone, Debug, Reflect)]
+pub struct GridPosition { pub x: u32, pub y: u32 }
+
+const LEVEL_LAYOUT : [[Tile;3];3] = [
+    [ Tile::Forest, Tile::Forest, Tile::Rocks],
+    [ Tile::FlatGround, Tile::Forest, Tile::FlatGround],
+    [Tile::FlatGround, Tile::FlatGround, Tile::FlatGround]
+];
+
+const UNITS : [(u32, u32, Unit);2] = [(0,1, Unit::Blue), (2,1, Unit::Red)];
+
+fn spawn_map(mut commands: Commands) {
+    for (y, row) in LEVEL_LAYOUT.iter().enumerate() {
+        for (x, tile) in row.iter().enumerate() {
+            commands.spawn((GridPosition { x: x as u32,y: y as u32}, tile.clone()));
+        }
+    }
+
+    for (x,y,unit) in UNITS {
+        commands.spawn((GridPosition { x, y, }, unit.clone()));
+    }
+}
+
+fn process_click(mut clicks: EventReader<Clicked>, mut commands: Commands, units: Query<(Entity, &Unit, &GridPosition)>, tiles: Query<(&Tile, &GridPosition)>, selected_units: Query<Entity, With<SelectedUnit>>) {
+    for click in clicks.read() {
+        match click{
+            Clicked::Unit { x, y, entity } => {
+                for entity in &selected_units {
+                    commands.entity(entity).remove::<SelectedUnit>();
+                }
+                if units.get(*entity).is_ok() {
+                    commands.entity(*entity).insert(SelectedUnit);
+                }
+            },
+            Clicked::Tile { x, y, x_offset, y_offset } => {
+                for unit in &selected_units {
+                    commands.entity(unit).insert(GridPosition { x: *x, y: *y});
+                }
+            },
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
 }
